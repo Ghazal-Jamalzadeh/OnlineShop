62b5a135716dbe2233fea53f92eeba6136cefb6a
Your Name <jmzd.developer94@gmail.com>
2021-08-04 22:35:38 +0430

    setup recycler view

--- app/src/main/java/com/jmzd/ghazal/onlineshop/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/onlineshop/MainActivity.kt
@@ -2,11 +2,33 @@ package com.jmzd.ghazal.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jmzd.ghazal.onlineshop.adapter.Adapter_recyclerview_shop
+import com.jmzd.ghazal.onlineshop.api.Api_shop
+import com.jmzd.ghazal.onlineshop.dataModel.Datamodel_shop
+import com.jmzd.ghazal.onlineshop.databinding.ActivityMainBinding
+import com.pnikosis.materialishprogress.ProgressWheel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var adapter: Adapter_recyclerview_shop
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Setup()
+    }
+    fun Setup(){
+        val api = Api_shop(applicationContext,binding.progressWheel )
+        api.Get_list(object : Api_shop.Getlist{
+            override fun list(list: List<Datamodel_shop>) {
+                binding.recyclerview.layoutManager= LinearLayoutManager(applicationContext)
+                adapter = Adapter_recyclerview_shop(applicationContext,list)
+                binding.recyclerview.adapter=adapter
+                binding.recyclerview.setHasFixedSize(true)
+            }
+        })
     }
 }
--- app/src/main/java/com/jmzd/ghazal/onlineshop/api/Api_shop.kt
+++ app/src/main/java/com/jmzd/ghazal/onlineshop/api/Api_shop.kt
@@ -1,7 +1,9 @@
 package com.jmzd.ghazal.onlineshop.api
 
 import android.content.Context
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
 import com.android.volley.Response
@@ -16,8 +18,10 @@ class Api_shop(val content: Context, val progressWheel: ProgressWheel):Config {
 
     fun Get_list(face:Getlist){
         var list=ArrayList<Datamodel_shop>()
-        val Jsarry= JsonArrayRequest(Baseurl,Response.Listener { response ->
+        val Jsarry= JsonArrayRequest(Baseurl, { response ->
             progressWheel.visibility= View.GONE
+            Log.d("shopError", response.toString())
+
             for(i in 0 until response.length()){
                 val jsonObject: JSONObject =response.getJSONObject(i)
                 var data=Datamodel_shop(jsonObject.getString("id"),jsonObject.getString("title"),
@@ -27,10 +31,12 @@ class Api_shop(val content: Context, val progressWheel: ProgressWheel):Config {
             }
             face.list(list)
 
-        }, Response.ErrorListener { error ->
+        }, { error ->
             progressWheel.visibility=View.GONE
+            Log.d("shopError", error.toString())
         })
-        Jsarry.setRetryPolicy(DefaultRetryPolicy(10000,Int.MAX_VALUE, Float.MAX_VALUE))
+        //Jsarry.retryPolicy = DefaultRetryPolicy(10000,Int.MAX_VALUE, Float.MAX_VALUE)
+        Jsarry.retryPolicy = DefaultRetryPolicy(10000,Int.MAX_VALUE, Float.MAX_VALUE)
         // ۱۰ ثانیه - هندل کردن اینترنت ضعیف و... که اگر پاسخ را از سرور دیر دریافت کردیم و... . مقادیر پیش فرض است
         Volley.newRequestQueue(content).add(Jsarry)
     }
@@ -38,6 +44,8 @@ class Api_shop(val content: Context, val progressWheel: ProgressWheel):Config {
     interface Getlist{
         fun list(list:List<Datamodel_shop>)
     }
+     //چرا به این اینترفیس احتیاج داریم؟
+    // چون داده ای که از سمت سرور دریافت می کنیم ممکن است کمی زمان بر باشد.
+    // داگر متد باشد ممکن است به مشکل بخوریم. مثلا داده را زودتر دریافت کنیم و null اتفاق بیفتد.
+    // به همین خاطر اینترفبیسی این وسط است که منتظر می ماند تا داده را دریافت کند و به محض دریافت داده آن را به main برمی گرداند.
 }
-
-//jsonArrayRequest -> 0 = get data from server / 1 = send data to server
